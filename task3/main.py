2df98501ec469d37396cccaced096c6da426c5d8
Константин Потекаев <kostya.potekaev@gmail.com>
2023-06-06 11:51:12 +0300

    backpack function is done

--- task3/main.py
+++ task3/main.py
@@ -1,4 +1,5 @@
 import string
+from itertools import product, combinations
 
 
 def deleteDuplicates(lst):  # lst - list of duplicate values
@@ -13,10 +14,26 @@ def popularWords(s):  # s - string (text)
     for word in lst_words:
         cnt_mas.append((s.count(word), word))
     cnt_mas.sort(reverse=True)
-    for i in range(min(10,len(cnt_mas))):
+    for i in range(min(10, len(cnt_mas))):
         print(cnt_mas[i])
 
 
-def backpack(d):    #d - dictionary of items
+def backpack(d, w):  # d - dictionary of items, w - max weight of items
+    lst_sets = []
+    items = list(d.keys())
+    for cnt_items in range(1,10**9):
+        flag = 0
+        for p in combinations(items, cnt_items):
+            lst = list(p)
+            itw = 0
+            for item in lst:
+                itw += d[item]
+            if itw <= w:
+                flag = 1
+                lst_sets.append(lst)
+        if not flag:
+            break
+    return lst_sets
 
 
+print(backpack({'aaa': 10, 'bbb': 100}, 1000))

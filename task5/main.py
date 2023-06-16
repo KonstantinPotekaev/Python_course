d4a22c7569ab92531712e0b2763c708588e9d3af
Константин Потекаев <kostya.potekaev@gmail.com>
2023-06-16 09:59:52 +0300

    chess_module done

--- task5/main.py
+++ task5/main.py
@@ -1,6 +1,6 @@
 from date_module import dateVerification
+from chess_modul import chess
 
 if __name__ == '__main__':
     print(dateVerification('11.02.2005'))
-
-
+    print(chess([0, 1, 2, 3, 4, 5, 6, 7], [0, 1, 2, 3, 4, 5, 6, 7]))
